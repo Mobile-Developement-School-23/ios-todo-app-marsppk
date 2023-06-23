f9c4a5369cd1b0e2f250e5db99e99405328a49e6
Maria Slepneva <marsppk2003@gmail.com>
2023-06-24 02:44:15 +0300

    ViewControllerUpdate

--- toDoList/FileCache.swift
+++ toDoList/FileCache.swift
@@ -10,6 +10,9 @@ import Foundation
 final class FileCache {
     private(set) var items = [TodoItem]()
     
+    
+    
+    
     func add(item: TodoItem) {
         if let index = items.firstIndex(where: { $0.id == item.id }) {
             items[index] = item
